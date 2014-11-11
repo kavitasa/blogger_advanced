56ae8388669c8e5376cb9f1c7c41eb98e1116fc3
worace <horace.d.williams@gmail.com>
2014-11-11 14:12:39 -0700

    handle missing articles by redirecting user to index page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,11 @@
 class ArticlesController < ApplicationController
   def show
-    @article = Article.find(params[:id])
+    begin
+      @article = Article.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      flash[:notice] = "Sorry, the article #{params[:id]} does not exist"
+      redirect_to articles_path
+    end
   end
 
   def index
